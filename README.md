dacf43b26c050e723c21b2562e7f8c99722293e4
tctrautman <tctrautman@gmail.com>
2019-03-29 15:40:29 -0700

    initialize repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tim's Portfolio Site
+
+This is the codebase for https://www.timtrautman.com.  It's built using [Gatsby.js](https://www.gatsbyjs.org/), which is a framework for building fast static sites using React and GraphQL.
+
+The main goal of this site is to help viewers understand who I am, what I've done, and what my work looks like.
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <div id="intro">
+                    <div>hey, I'm Tim</div>
+                    <div>Photo</div>
+                </div>
+                <div id="content">
+                    text or portfolio
+                </div>
+                <div id="footer">
+                    buttons
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
