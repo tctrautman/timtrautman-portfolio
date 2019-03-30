99a33d5fd6c4b0d0f0ae6d2cab1321338ceda4d2
tctrautman <tctrautman@gmail.com>
2019-03-29 18:48:02 -0700

    Add styling and photo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background: #707070;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 100;
+    font-size: 54px;
+    color: #FFDA9D;
+    text-align: center;
+    height: 100%;
+    padding: 0px;
+    margin: 20px;
+    padding: 20px;
+
+    img {
+        border-radius: 50%;
+        max-height: 250px;
+        max-height: 250px
+    }
+`;
 
 const Intro = props => (
-    <div id="intro">
+    <Container>
+        <img src="https://s3.amazonaws.com/tt-personal-site/timpicture.jpg" alt="Photo of Tim"/>
         <div>hey, I'm Tim</div>
-        <div>Photo</div>
-    </div>
+    </Container>
 );
 
 export default Intro;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import Intro from '../components/Intro';
 import Content from '../components/Content';
 import Footer from '../components/Footer';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background: #707070;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 200;
+    color: #FFDA9D;
+    text-align: center;
+    width: 100%;
+`;
 
 class Home extends Component {
     constructor() {
@@ -11,18 +21,24 @@ class Home extends Component {
         }
     }
 
-    _updateContent() {
-        const newContent = this.state.content === 'home' ? 'portfolio' : 'home';
-        this.setState({'content': newContent});
+    _setHome = () => {
+        this.setState({'content': 'home'});
+    }
+
+    _setPortfolio = () => {
+        this.setState({'content': 'portfolio'})
     }
 
     render() {
         return (
-            <div>
+            <Container>
                 <Intro />
                 <Content currentContent={this.state.content} />
-                <Footer />
-            </div>
+                <Footer
+                    setHome={this._setHome}
+                    setPortfolio={this._setPortfolio}
+                />
+            </Container>
         );
     }
 }
