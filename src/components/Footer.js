0bfa6f87997f9b76b89ff1b9a5e49f0a38ce809a
tctrautman <tctrautman@gmail.com>
2019-03-29 19:32:17 -0700

    Add icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { FaHome, FaPaintBrush, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { GoMarkGithub } from 'react-icons/go';
 
 const Footer = props => (
     <div id="footer">
-        <a onClick={props.setPortfolio}>Portfolio</a>
-        <a onClick={props.setHome}>Home</a>
+        <a onClick={props.setPortfolio}>
+            <FaPaintBrush />
+            Portfolio
+        </a>
+        <a onClick={props.setHome}>
+            <FaHome  />
+            Home
+        </a>
+        
+        <GoMarkGithub />
+        <FaLinkedin />
+        <FaTwitter />
     </div>
 );
 
