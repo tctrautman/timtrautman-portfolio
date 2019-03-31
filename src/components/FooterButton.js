8cd3f6b042c7f9debdfaf2d13a66342ce59ffd06
tctrautman <tctrautman@gmail.com>
2019-03-30 21:37:51 -0700

    Rename Button to FooterButton

--- src/components/FooterButton.js
+++ src/components/FooterButton.js
@@ -8,10 +8,14 @@ const IconContainer = styled.div`
     border-width: 1px;
     border-style: solid;
     border-radius: 20%;
-    margin: 10px;
+    margin-top: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 13px;
     max-width: 70px;
     display: inline-block;
-
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    
     div {
         padding-top: 7px;
         padding-bottom: 7px;
@@ -29,7 +33,6 @@ const IconContainer = styled.div`
 `;
 
 const Button = props => {
-    const activeIcon = props.icon;
     return (
         <IconContainer>
             <a onClick={props.handler} href={props.path}>
