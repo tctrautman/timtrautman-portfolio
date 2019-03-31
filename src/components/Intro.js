83e9c9c12d27a675e6d0eb7a86ec20f4b0c2c112
tctrautman <tctrautman@gmail.com>
2019-03-30 21:41:57 -0700

    Add shadow to picture, remove 'photo' from img tag's alt

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,18 +9,20 @@ const Container = styled.div`
     text-align: center;
     height: 100%;
     padding: 0px;
+    margin: 0px;
 
     img {
         border-radius: 50%;
         max-height: 250px;
         max-height: 250px;
-        margin-top: 50px;
+        margin-top: 10px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
 `;
 
 const Intro = props => (
     <Container>
-        <img src="https://s3.amazonaws.com/tt-personal-site/timpicture.jpg" alt="Photo of Tim"/>
+        <img src="https://s3.amazonaws.com/tt-personal-site/timpicture.jpg" alt="Tim"/>
         <div>hey, I'm Tim</div>
     </Container>
 );
