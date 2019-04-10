895e44eb8f2487688ed19a3e5687dbc8e5ebe468
tctrautman <tctrautman@gmail.com>
2019-04-09 20:25:15 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,17 @@
 module.exports = {
-    plugins: [`gatsby-plugin-styled-components`],
+    plugins: [
+      `gatsby-plugin-styled-components`,
+      {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-68633468-1",
+          head: false,
+          anonymize: true,
+          respectDNT: true,
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    ],
+
   }
   
