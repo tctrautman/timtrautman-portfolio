d0cb1930182c7c88546c1571fd19369ab2d789ad
tctrautman <tctrautman@gmail.com>
2019-10-23 17:27:44 -0700

    Update projects

--- src/projects.js
+++ src/projects.js
@@ -1,64 +1,86 @@
 const projects = [
-    {
-        title: "Buy My Mousse",
-        description: "A full-stack ecommerce app that lets users create an account, buy, and sell items.  I built this as a way to use GraphQL for the first time.",
-        builtUsing: ["React", "Apollo", "GraphQL", "Express (Yoga)", "Next.js"],
-        type: 'full-stack',
-        feGithub: "https://github.com/tctrautman/buymymousse-frontend",
-        beGithub: "https://github.com/tctrautman/buymymousse-backend",
-        publicLink: null,
-        picSrc: "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744366/Screen_Shot_2019-04-07_at_9.32.15_PM.png",
-        teamEffort: false,
-        flipped: false
-    },
-    {
-        title: "Event Planner",
-        description: "This project was a fun way to get familiar with CSS grid and think about packing algorithms.",
-        builtUsing: ["React", "Gatsby.js", "Styled Components", "CSS grid", "Netlify"],
-        type: 'front-end',
-        feGithub: "https://github.com/tctrautman/event-planner",
-        beGithub: null,
-        publicLink: "https://romantic-shannon-fd95da.netlify.com/",
-        picSrc: "https://res.cloudinary.com/dna5twsdm/image/upload/v1556242845/Screen_Shot_2019-04-25_at_6.39.28_PM.png",
-        teamEffort: false,
-        flipped: false
-    },
-    {
-        title: "Curve fitting",
-        description: "This is a front-end application used to demonstrate core concepts of probabilistic programming.  I was part of the engineering team that built this at MIT.",
-        builtUsing: ["ClojureScript", "Quil", "Metaprob"],
-        type: 'front-end',
-        feGithub: "https://github.com/probcomp/curve-fitting",
-        beGithub: null,
-        publicLink: null,
-        picSrc: "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744361/Screen_Shot_2019-04-08_at_10.24.26_AM.png",
-        teamEffort: true,
-        flipped: false
-    },
-    {
-        title: "Portfolio site",
-        description: "This is the source code for the site you're on right now.  I built it from scratch using Gatsby.js.",
-        builtUsing: ["React", "Gatsby.js", "Styled Components", "Netlify"],
-        type: 'front-end',
-        feGithub: "https://github.com/tctrautman/timtrautman-portfolio",
-        beGithub: null,
-        publicLink: null,
-        picSrc: "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744362/Screen_Shot_2019-04-07_at_9.36.30_PM.png",
-        teamEffort: false,
-        flipped: false
-    },
-    {
-        title: "Paw Partners",
-        description: "I adpated this site from a Gatsby.js template, integrated the /survey page with typeform, and used FB ads to conduct a survey of ~150 animal shelter owners.",
-        builtUsing: ["React", "Gatsby.js", "Typeform integration"],
-        type: 'front-end',
-        feGithub: null,
-        beGithub: null,
-        publicLink: "https://pawpartners.io",
-        picSrc: "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744380/Screen_Shot_2019-04-07_at_6.41.07_PM.png",
-        teamEffort: false,
-        flipped: false
-    }
-];
+  {
+    title: "Buy My Mousse",
+    description:
+      "A full-stack ecommerce app that lets users create an account, buy, and sell items.  I built this as a way to use GraphQL for the first time.",
+    builtUsing: ["React", "Apollo", "GraphQL", "Express (Yoga)", "Next.js"],
+    type: "full-stack",
+    feGithub: "https://github.com/tctrautman/buymymousse-frontend",
+    beGithub: "https://github.com/tctrautman/buymymousse-backend",
+    publicLink: null,
+    picSrc:
+      "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744366/Screen_Shot_2019-04-07_at_9.32.15_PM.png",
+    teamEffort: false,
+    flipped: false,
+  },
+  {
+    title: "Event Planner",
+    description:
+      "This project was a fun way to get familiar with CSS grid and think about packing algorithms.",
+    builtUsing: [
+      "React",
+      "Gatsby.js",
+      "Styled Components",
+      "CSS grid",
+      "Netlify",
+    ],
+    type: "front-end",
+    feGithub: "https://github.com/tctrautman/event-planner",
+    beGithub: null,
+    publicLink: "https://romantic-shannon-fd95da.netlify.com/",
+    picSrc:
+      "https://res.cloudinary.com/dna5twsdm/image/upload/v1556242845/Screen_Shot_2019-04-25_at_6.39.28_PM.png",
+    teamEffort: false,
+    flipped: false,
+  },
+  {
+    title: "Curve fitting",
+    description:
+      "This is a front-end application used to demonstrate core concepts of probabilistic programming.  I was part of the engineering team that built this at MIT.",
+    builtUsing: ["ClojureScript", "Quil", "Metaprob"],
+    type: "front-end",
+    feGithub: "https://github.com/probcomp/curve-fitting",
+    beGithub: null,
+    publicLink: null,
+    picSrc:
+      "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744361/Screen_Shot_2019-04-08_at_10.24.26_AM.png",
+    teamEffort: true,
+    flipped: false,
+  },
+  {
+    title: "Portfolio site",
+    description:
+      "This is the source code for the site you're on right now.  I built it from scratch using Gatsby.js.",
+    builtUsing: ["React", "Gatsby.js", "Styled Components", "Netlify"],
+    type: "front-end",
+    feGithub: "https://github.com/tctrautman/timtrautman-portfolio",
+    beGithub: null,
+    publicLink: null,
+    picSrc:
+      "https://res.cloudinary.com/dna5twsdm/image/upload/v1554744362/Screen_Shot_2019-04-07_at_9.36.30_PM.png",
+    teamEffort: false,
+    flipped: false,
+  },
+  {
+    title: "Serverless Todos",
+    description:
+      "This is a 'serverless' todo app, maintaining state without a backend.",
+    builtUsing: [
+      "React",
+      "Gatsby.js",
+      "Netlify",
+      "FaunaDB",
+      "AWS Lambda (via Netlify)",
+    ],
+    type: "full-stack",
+    feGithub: "https://github.com/tctrautman/netlify-faunadb-todo",
+    beGithub: null,
+    publicLink: "https://priceless-wiles-72922d.netlify.com/",
+    picSrc:
+      "https://res.cloudinary.com/dna5twsdm/image/upload/v1571875799/Screen_Shot_2019-10-23_at_5.09.18_PM.png",
+    teamEffort: false,
+    flipped: false,
+  },
+]
 
-export default projects;
+export default projects
